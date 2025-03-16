e53b7ff9303631662320acbccea6c603ea5d61e6
Nkundimana Adeodatus <nkundiman3@gmail.com>
2025-03-16 19:16:53 +0200

    create a working interface to interact with the AI agent

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
 import { Mistral } from "@mistralai/mistralai";
+import readline from "node:readline"
+import dotenv from "dotenv"
 
-const apiKey = import.meta.env.VITE_MISTRAL_API
-console.log(apiKey);
+dotenv.config()
+
+const apiKey = process.env.VITE_MISTRAL_API
+console.log('Type "exit", ctrl+c or ctrl+d to exit');
+console.log('How can I Help?')
 
 const mistralClient = new Mistral({apiKey: apiKey})
 
+// Function to create an interface to read and write data.
+let userInput
+let result = ''
+const messages = []
+const rw = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+async function interactWithUser() {
+
+    
+        const prompt = await new Promise((resolve) => {
+            rw.question( "\nAsk AI: \n", (answer) => {
+                if(answer !== '') {
+                    resolve(answer)
+                } else {
+                    console.log("Please enter a prompt")
+                    interactWithUser()
+                }
+            })
+        })
+
+        if(prompt.toLowerCase() === "exit"){
+            console.log("Good bye😊!")
+            rw.close()
+            return
+        }
+
+        userInput = prompt
+        messages.push()
+        // Ensure the debounce function finishes before continuing
+        await generateResponse()
+        interactWithUser()
+    
+        
+    
+
+
+}
+interactWithUser()
+
 async function agent(query) {
     // message array to store the conversation with the agent
-    const messages = [
-        {role: 'user', content: query}
-    ]
+    messages.push({role: 'user', content: query})
+    // const messages = [
+    //     {role: 'user', content: query}
+    // ]
     
     //response from the agent.
     const response =  await mistralClient.chat.stream({
@@ -22,11 +69,17 @@ async function agent(query) {
 
     // Handling the chunks of response
     try{
-        let result = ''
+        
         for await(const chunk of response){
-            result += chunk.data.choices[0].delta.content
-            console.log(result)
+            process.stdout.write(chunk.data.choices[0].delta.content)
+            if(chunk.data.choices[0].finishReason === 'stop'){
+                result += `${chunk.data.choices[0].delta.content}`
+                console.log()
+            } else {
+                result += chunk.data.choices[0].delta.content
+            }
         }
+        messages.push({role: 'assistant', content: result})
         return result
     }
     catch(err){
@@ -37,20 +90,19 @@ async function agent(query) {
 
 // Function to call the agent while adding some other code to be run whe the agent is called
 async function generateResponse() {
-    const responseFromAgent = await agent('hello')
-    console.log('Final response', responseFromAgent)
+    await agent(userInput)
 }
 
 // debounce function to handle the rate limit of the mistral api call
 function debounce(func, delay) {
     let timeoutId
-    return function(...args) {
+    return async function(...args) {
         const context = this
         if(timeoutId) clearTimeout(timeoutId)
-        timeoutId = setTimeout(func.apply(context, args),delay)
+        timeoutId = setTimeout(() => func.apply(context, args),delay)
+        if(args[0]) args[0]()
     }
 }
 
 const debounceOutput = debounce(generateResponse, 1000)
 
-debounceOutput()
